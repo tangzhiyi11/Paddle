1fc07717b069679b90cf8b5aa41d0d64b596e1f5
卖鱼的哲学 <tangzhiyi11@users.noreply.github.com>
2021-06-01 15:13:32 +0800

    update

--- paddle/fluid/imperative/tracer.cc
+++ paddle/fluid/imperative/tracer.cc
@@ -205,14 +205,6 @@ void Tracer::TraceOp(const std::string& type, const NameVarBaseMap& ins,
 
   if (ComputeRequiredGrad(new_ins, outs, trace_backward)) {
     CreateGradOpNode(*op, new_ins, outs, attrs, place, inplace_map);
-    // Note(tangzhiyi01): add by tangzhiyi01
-    std::cout << "Attention, this op " << type << " create a grad node!"
-              << std::endl;
-    std::cout << "inplace map:" << std::endl;
-    for (auto& s : inplace_map) {
-      std::cout << "key: " << s.first << " value: " << s.second << std::endl;
-    }
-    printf("\n");
   } else {
     VLOG(3) << "No Grad to track for Op: " << type;
   }
